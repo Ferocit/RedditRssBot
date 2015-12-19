191600c37f4c1ef72cb7b240b5d646295de6fca9
Feroc <github@feroc.de>
2015-12-19 23:54:30 +0100

    More test code for oauth
    Working authentication!
    
    Added properties file for credentials

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,32 @@
 import com.sun.syndication.feed.synd.SyndFeed;
 import com.sun.syndication.io.FeedException;
+import net.dean.jraw.ApiException;
+import net.dean.jraw.RedditClient;
+import net.dean.jraw.fluent.FluentRedditClient;
+import net.dean.jraw.models.Submission;
+import reddit.apiwrapper.RedditClientFactory;
 import rss.RomeHelper;
 import rss.RssNews;
 import rss.RssNewsEntry;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Main {
 
-    public static void main(String [] args) throws IOException, FeedException {
+    public static void main(String [] args) throws IOException, FeedException, InterruptedException, ApiException {
+
         RssNews rss = new RssNews("http://www.rawstory.com/rs/feed/");
-        List<RssNewsEntry> rssNewsEntries = rss.getRssNewsEntries("ReLiGiO");
+        List<RssNewsEntry> rssNewsEntries = rss.getRssNewsEntries("a");
 
-        for (RssNewsEntry rssNewsEntry : rssNewsEntries) {
-            System.out.println(rssNewsEntry.getTitle());
-        }
+        RedditClientFactory auth = new RedditClientFactory();
+        RedditClient reddit = auth.authenticate();
 
+        FluentRedditClient fluent = new FluentRedditClient(reddit);
+
+        Submission submission = fluent.subreddit("News4AtheistsBotTest").submit("http://www.google.de", "title");
     }
-
 
 }
---
+++ src/main/java/reddit/apiwrapper/RedditClientFactory.java
@@ -0,0 +1,44 @@
+package reddit.apiwrapper;
+
+import net.dean.jraw.RedditClient;
+import net.dean.jraw.http.UserAgent;
+import net.dean.jraw.http.oauth.Credentials;
+import net.dean.jraw.http.oauth.OAuthData;
+import net.dean.jraw.http.oauth.OAuthException;
+import net.dean.jraw.http.oauth.OAuthHelper;
+import net.dean.jraw.models.LoggedInAccount;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Created by Mathias on 19.12.2015.
+ */
+public class RedditClientFactory {
+
+    public RedditClient authenticate() {
+        try {
+            Properties properties = getProperties();
+
+            UserAgent myUserAgent = UserAgent.of("Windows", "RedditAtheismNews", "0.1", "Feroc");
+            RedditClient redditClient = new RedditClient(myUserAgent);
+            Credentials credentials = Credentials.script(properties.getProperty("user"), properties.getProperty("password"), properties.getProperty("app_id"), properties.getProperty("app_secret"));
+            OAuthData authData = redditClient.getOAuthHelper().easyAuth(credentials);
+            redditClient.authenticate(authData);
+            LoggedInAccount me = redditClient.me();
+            System.out.println("Logged in: " + me.getFullName());
+            return redditClient;
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return null;
+    }
+
+    private Properties getProperties() throws IOException {
+        Properties properties = new Properties();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("credentials.properties");
+        properties.load(inputStream);
+        return properties;
+    }
+}
