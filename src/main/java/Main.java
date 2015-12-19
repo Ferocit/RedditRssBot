fb4320b81cdb769e8f92baab70c46009b37780cd
Feroc <github@feroc.de>
2015-12-19 19:01:58 +0100

    (RAN-1) Just some timer test code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,17 +6,38 @@ import rss.RssNewsEntry;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Main {
 
-    public static void main(String [] args) throws IOException, FeedException {
+    public static void main(String [] args) throws IOException, FeedException, InterruptedException {
+
+        Timer timer = new Timer();
+        final TimerTask updateRssTimer = new TimerTask() {
+            @Override
+            public void run() {
+                updateRss();
+            }
+        };
+        timer.scheduleAtFixedRate(updateRssTimer, 0, 2*60*1000);
+
+        boolean running = true;
+        while (running) {
+            Thread.sleep(5000);
+        }
+
+    }
+
+    public static void updateRss() {
+        System.out.println("updating rss...");
         RssNews rss = new RssNews("http://www.rawstory.com/rs/feed/");
-        List<RssNewsEntry> rssNewsEntries = rss.getRssNewsEntries("ReLiGiO");
+        List<RssNewsEntry> rssNewsEntries = rss.getRssNewsEntries("a");
 
         for (RssNewsEntry rssNewsEntry : rssNewsEntries) {
             System.out.println(rssNewsEntry.getTitle());
         }
-
+        System.out.println("done.");
     }
 
 
