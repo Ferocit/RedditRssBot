a57a4a8ec9bf606a1ccf0b8af0b9bdd95949f7c7
Feroc <github@feroc.de>
2015-12-23 14:24:44 +0100

    Moved stuff around

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import net.dean.jraw.ApiException;
 
 import javax.xml.bind.JAXBException;
 
-public class StartHere {
+public class Main {
 
     public static void main(String [] args) throws ApiException, JAXBException {
         RedditBot redditBot = new RedditBot();
